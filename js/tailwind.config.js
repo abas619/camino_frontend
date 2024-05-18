3b8d8de0ebebea88dc20ada1bc7ca13c0bed8501
Abas Dolati <lostangel619@gmail.com>
2024-05-18 14:37:20 +0330

    + master-panel index, reservation

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -5,12 +5,18 @@ tailwind.config = {
     //   primary: "#fda621",
     //   secondary: "#1a8ac9",
     //   accent: "#080067",
+    //   success: "#00bd79",
+    //   error: "#ff3333",
+    //   warning: "#ffd700",
+    //   info: "#009cf2",
     //   textDark: "#3c3c3c",
     //   textGrey: "#636063",
     //   textSilver: "#9e9e9e",
     //   borderColor: "#d2d2d2",
     //   background: "#fff",
     //   background_light: "#f2f2f3",
+    //   white: "#ffffff",
+    //   transparent: "transparent",
     // },
     container: {
       screens: {
