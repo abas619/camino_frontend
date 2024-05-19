e2fb0aa5c175433a4c44ac39ff5f24a7540e0b76
Abas Dolati <lostangel619@gmail.com>
2024-05-19 15:28:21 +0330

    + comment, profile

---
+++ plugins/dropzone/dropzone.init.js
@@ -0,0 +1,21 @@
+Dropzone.options.fileImagesUploadCover = {
+  paramName: "file",
+  maxFilesize: 0.5,
+  url: "/your-upload-url",
+  parallelUploads: 1,
+  uploadMultiple: false,
+  acceptedFiles: "image/*",
+  // accept: function (file, done) {
+  //     if (file.name == "justinbieber.jpg") {
+  //         done("Naha, you don't.");
+  //     }
+  //     else { done(); }
+  // }
+};
+
+//   paramName: "file",
+//   maxFilesize: 0.4,
+//   url: "/your-upload-url",
+//   parallelUploads: 1,
+//   uploadMultiple: false,
+//   acceptedFiles: "image/*",
